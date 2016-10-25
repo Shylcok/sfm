c5a9379cd472f534256852b45a40989d68ddf389
developer <developer@sfm.com>
2016-10-25 10:29:33 +0800

    服务器配置

--- setting_prod.py
+++ setting_prod.py
@@ -11,3 +11,32 @@
 @file: setting_prod.py
 @time: 16/10/12 下午2:39
 """
+
+
+CONFIG = {
+    'DEBUG': False,
+
+    'HTTP': {
+        'HOST': '0.0.0.0',
+        'PORT': 8089,
+    },
+
+    'DATABASE': {
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWD': 'qwrYlksfnDqrfsa*3weaew',
+        'NAME': 'sfm',
+    },
+
+    'LOG_PATH': './sfm-8089.log',
+    'pay': {
+            'app_id': 'app_9CO8q1TGCK00P44G',
+            'api_key': 'sk_live_L0CqXLf1SyfL0GO4a9zv1yHS',
+            },
+
+    'token_secret_key': 'qksytijhjhhdhha%&*&O&&(*',
+    'AESKEY': 'ovgNwKHpqmdOTYgI',
+
+
+}
--- start_sfm.sh
+++ start_sfm.sh
@@ -1,4 +1,4 @@
 source /data/suyuan/sfm_env/bin/activate
 #export PYTHONPATH=/data/htapp/hera/
-export SFM_ENV=DEV
+export SFM_ENV=PROD
 exec python sfm.py --log-rotate-mode=time --log-rotate-when=midnight $*
