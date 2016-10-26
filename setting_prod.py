cc50126e426ca4b7344b6300edafafe9179398f2
suyuan <suyuan@55HaiTao.com>
2016-10-26 14:00:04 +0800

    redis

--- setting_prod.py
+++ setting_prod.py
@@ -21,6 +21,12 @@ CONFIG = {
         'PORT': 8089,
     },
 
+    'SMS_REDIS': {  # 短信验证码
+        'HOST': '127.0.0.1',
+        'PORT': 6379,
+        'DB': 4
+    },
+
     'DATABASE': {
         'HOST': 'localhost',
         'PORT': 3306,
